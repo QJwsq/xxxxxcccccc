65b6d2837c33624a12e3a8f89da0ff5dc191d979
Until  You <2062906281@qq.com>
2017-09-07 18:48:32 +0800

    项目搭建

---
+++ FanTanJu/FanTanJu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FanTanJu
+//
+//  Created by Until  You on 2017/9/7.
+//  Copyright © 2017年 Until  You. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
